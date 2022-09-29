515fa945a240e8c45e3598b18336c02e4299a89d
ahmadrzq <54834305+ahmadrzq@users.noreply.github.com>
2022-09-30 05:53:44 +0700

    Menrapkan localStorage pada Kalkulator

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -88,15 +88,24 @@ function performCalculation() {
 
     let result = 0;
     if (calculator.operator === '+') {
-        result =  parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
-    } else if(calculator.operator === 'x'){
-        result =  parseInt(calculator.firstNumber) * parseInt(calculator.displayNumber);
-    } else if(calculator.operator === ':'){
-        result =  parseInt(calculator.firstNumber) / parseInt(calculator.displayNumber);
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else if (calculator.operator === 'x') {
+        result = parseInt(calculator.firstNumber) * parseInt(calculator.displayNumber);
+    } else if (calculator.operator === ':') {
+        result = parseInt(calculator.firstNumber) / parseInt(calculator.displayNumber);
     } else {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
-
+    
+    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
 }
 
